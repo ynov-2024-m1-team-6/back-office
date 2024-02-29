bf359073e9ea96dde67132fef3dca97784e3a31d
babouslt <bastienselot@gmail.com>
2024-02-29 16:01:37 +0100

    feat(add): delete, get by id and update user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Body, Param, Delete, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from '@prisma/client';
 
@@ -18,5 +9,25 @@ export class UserController {
   @Get('/')
   async getAllUsers(): Promise<{ message: string; data: User[] }> {
     return this.userService.getAllUsers();
+  }
+
+  @Get('/:id')
+  async getUserById(
+    @Param('id') id: string,
+  ): Promise<{ message: string; data: User }> {
+    return this.userService.getUserById(parseInt(id));
+  }
+
+  @Put('/:id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() data: User,
+  ): Promise<{ message: string; data: User }> {
+    return this.userService.updateUser(parseInt(id), data);
+  }
+
+  @Delete('/:id')
+  async deleteUser(@Param('id') id: string): Promise<{ message: string }> {
+    return this.userService.deleteUser(parseInt(id));
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -15,4 +15,63 @@ export class UserService {
       throw error; // You may want to handle errors in a more sophisticated way
     }
   }
+
+  async getUserById(id: number): Promise<{ message: string; data: User }> {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id },
+      });
+
+      if (!user) {
+        throw new Error('User not found');
+      }
+      return { message: 'success', data: user };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateUser(
+    id: number,
+    data: User,
+  ): Promise<{ message: string; data: User }> {
+    try {
+      const existingUser = await prisma.user.findUnique({
+        where: { id },
+      });
+
+      if (!existingUser) {
+        throw new Error('User not found for updating');
+      }
+
+      const updatedUser = await prisma.user.update({
+        where: { id },
+        data,
+      });
+
+      return { message: 'success', data: updatedUser };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteUser(id: number): Promise<{ message: string }> {
+    try {
+      const existingUser = await prisma.user.findUnique({
+        where: { id },
+      });
+
+      if (!existingUser) {
+        throw new Error('User not found for deleting');
+      }
+
+      await prisma.user.delete({
+        where: { id },
+      });
+
+      return { message: 'success' };
+    } catch (error) {
+      throw error;
+    }
+  }
 }
